1fd761d19a112da29d98570d882629e4652c9160
logologologo000 <60058140+logologologo000@users.noreply.github.com>
2021-02-13 16:14:57 +0700

    table

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.master')
+@section('content')
+
+<h1>หมวดหมู่ข่าว</h1>
+
+<table style="background-color: rgb(229, 190, 190); ">
+    <thead>
+        <tr>
+            <td>ลำดับ</td>
+            <td>ชื่อหมวดหมู่</td>
+            <td>สร้างเมื่อ</td>
+            <td>จัดการ</td>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach($categories as $item)
+        <tr>
+            <td>{{$item->id}}</td>
+            <td>{{$item->name}}</td>
+            <td>{{$item->created_at}}</td>
+            <td>oo</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection;
