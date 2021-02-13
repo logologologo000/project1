0d91c5a5471d63474c304b0351b60c9d711311a0
logologologo000 <60058140+logologologo000@users.noreply.github.com>
2021-02-13 14:08:27 +0700

    last

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'] );
 Route::get('/about', [HomeController::class, 'about'] );
 Route::get('/contact', [HomeController::class, 'contact'] );
+Route::get('/hello/{name}/{age}', [HomeController::class, 'hello'] );
 
 
 
