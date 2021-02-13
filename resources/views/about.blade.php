0e4d4d9d6e5f57c74e01b436cce498837cf5e9d5
logologologo000 <60058140+logologologo000@users.noreply.github.com>
2021-02-13 11:42:48 +0700

    เพิ่ม About contact web

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    Laravel
+    ABOUT
 </body>
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    Laravel
+    CONTACT
 </body>
 </html>
