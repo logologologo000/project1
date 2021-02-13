2a4307195bc1eefc56f718619766f8186b8dea25
logologologo000 <60058140+logologologo000@users.noreply.github.com>
2021-02-13 14:28:18 +0700

    masterpage

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="/">Welcome</a> | <a href="/about">About</a> | <a href="/contact">Contact</a> <br><br>
+
+    @yield("content")
+
+
+    <hr>
+    <center>&copy; 2021 pannawit</center>
+</body>
+</html>
