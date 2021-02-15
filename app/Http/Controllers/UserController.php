ca213f4682e806be1dade963f37dd16ed4cf5f4d
logologologo000 <60058140+logologologo000@users.noreply.github.com>
2021-02-15 08:32:45 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,7 +55,7 @@ class UserController extends Controller
         $name = $request->input('name');
         $username = $request->input('username');
         $email = $request->input('email');
-        $pp = $request->input('password');
+        $p = $request->input('password');
 
         $users = User::find($id);
 
